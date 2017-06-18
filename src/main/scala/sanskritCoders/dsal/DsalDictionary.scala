1a35e08d03766153d698836a6268bda3ea490390
vishvAsaH <vishvas.vasuki@gmail.com>
2017-06-17 22:38:27 -0700

    Add DCS scraper and database skeleton.

--- src/main/scala/sanskritCoders/dsal/DsalDictionary.scala
+++ src/main/scala/sanskritCoders/dsal/DsalDictionary.scala
@@ -1,5 +1,7 @@
 package sanskritCoders.dsal
 
 class DsalDictionary {
-
+// Scrape links to entries, as in:
+//  http://dsalsrv02.uchicago.edu/cgi-bin/philologic/search3advanced?dbname=burrow&searchdomain=headwords&display=utf8
+//  Visit each page (http://dsalsrv02.uchicago.edu/cgi-bin/philologic/getobject.pl?c.0:1:2396.burrow) and dump an entry.
 }
